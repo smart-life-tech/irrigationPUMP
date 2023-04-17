6dc7d1f5349c4afb151f585ff0a5a1190da419b2
smart-life-tech <christlightworld@gmail.com>
2023-04-17 19:27:14 +0100

    modified:   speedControl/speedControl.ino

--- speedControl/speedControl.ino
+++ speedControl/speedControl.ino
@@ -6,20 +6,21 @@ int pwm = 6;
 int in1 = 5;
 int in2 = 4;
 unsigned long t2 = 0;
-float setSpeed = 1500;
-float velocity;
+float setSpeed = 1500.00;
+float velocity = 0.00;
 bool read = false;
 // int vkmh = (100*3600)/1000;
 int counter = 0;
 bool clear, count1, count2 = true;
 void setup()
 {
+  Serial.begin(9600);
   pinMode(IR1, INPUT_PULLUP);
   pinMode(IR2, INPUT);
   pinMode(pwm, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-Serial.println("moving forward, more speed");
+  Serial.println("moving forward, more speed");
   analogWrite(pwm, 255);
   digitalWrite(in2, HIGH);
   digitalWrite(in1, LOW);
@@ -29,7 +30,7 @@ Serial.println("moving forward, more speed");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   delay(5000);
-  Serial.begin(9600);
+
 }
 
 void loop()
@@ -80,7 +81,7 @@ float getSpeed()
   {
     velocity = t2 - t1;
     velocity = velocity / 1000;        // convert millisecond to second for timig
-    velocity = (0.2 / velocity) * 3.6; // km/s
+    velocity = (0.2 / velocity) * 3.6 * 1000; // km/s
     Serial.print(velocity * 1000);
     Serial.println(" m/hr");
     delay(500);
@@ -95,8 +96,8 @@ float getSpeed()
 void controlMotor(float speed)
 {
   if (speed > 0)
-  {
-    if (speed > setSpeed + 200)
+  { Serial.println(speed);
+    if (speed > setSpeed + 400.00 )
     {
       // delay(5000);
       Serial.println("moving forward, more speed");
@@ -104,7 +105,7 @@ void controlMotor(float speed)
       digitalWrite(in2, HIGH);
       digitalWrite(in1, LOW);
     }
-    else if (speed < setSpeed - 200)
+    else if (speed < setSpeed - 300.00)
     {
       Serial.println("moving backward, less speed");
       analogWrite(pwm, 255);
