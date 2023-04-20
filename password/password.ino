652bdb6d4c2c1089528e0e429a98b7d5e0a1d010
smart-life-tech <christlightworld@gmail.com>
2023-04-20 15:30:18 +0100

    modified:   password/password.ino

--- password/password.ino
+++ password/password.ino
@@ -1014,7 +1014,7 @@ float getSpeed()
   {
     velocity = t2 - t1;
     velocity = velocity / 1000;               // convert millisecond to second for timig
-    velocity = (0.2 / velocity) * 3.6 * 1000; // km/s
+    velocity = (0.25 / velocity) * 3.6 * 1000; // km/s
     Serial.println("velocity/speed: ");
     Serial.print(velocity);
     speeding = velocity;
