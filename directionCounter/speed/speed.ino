47f31b9b914f7ae3a9f7e4e662cb4b1608755be7
smart-life-tech <christlightworld@gmail.com>
2023-04-22 22:51:54 +0100

    debugging on the interuppt modified:   directionCounter/speed/speed.ino

--- directionCounter/speed/speed.ino
+++ directionCounter/speed/speed.ino
@@ -7,63 +7,71 @@ bool count1, clear = true;
 bool count2, read = false;
 int counter = 0;
 // int vkmh = (100*3600)/1000;
-
+bool done = true;
+unsigned long lastMillis = 0;
 void setup()
 {
 
-    pinMode(IR1, INPUT);
-    attachInterrupt(0, reads, FALLING);
-    Serial.begin(9600);
+  pinMode(IR1, INPUT_PULLUP);
+  attachInterrupt(0, reads, RISING);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-    if (counter == 1)
-    {
-        if (count1)
-        {
-            t1 = millis();
-            Serial.println(t1);
-            read = false;
-            count2 = true;
-            count1 = false;
-        }
-    }
-    else if (counter >= 2)
-    {
-        if (count2)
-        {
-            t2 = millis();
-            Serial.println(t2);
-            read = true;
-            count1 = true;
-            count2 = false;
-        }
-        counter = 0;
-    }
-    if (read)
-    {
-        velocity = t2 - t1;
-        velocity = velocity / 1000;                 // convert millisecond to second for timig
-        velocity = (0.155 / velocity) * 3.6 * 1000; // km/s
-        Serial.println("velocity/speed: ");
-        Serial.print(velocity);
-        speeding = velocity;
-        Serial.println(" m/hr");
-        delay(500);
-        read = false;
-    }
+  if (count2)
+  {
+    t2 = millis();
+    //Serial.print("t2 ");
+    //Serial.println(t2);
+    read = true;
+    done = true;
+    count1 = false;
+    count2 = false;
+  }
+  if (count1)
+  {
+    t1 = millis();
+    //Serial.print("t1 ");
+    //Serial.println(t1);
+    done = true;
+    count1 = false;
+    count2 = false;
+  }
 
+  if (read)
+  {
+    velocity = t2 - t1;
+    velocity = velocity / 1000;                 // convert millisecond to second for timig
+    velocity = (0.155 / velocity) * 3.6 * 1000; // km/s
+    Serial.println("velocity/speed: ");
+    Serial.print(velocity);
+    speeding = velocity;
+    Serial.println(" m/hr");
     delay(500);
+    read = false;
+  }
 }
 void reads()
 {
-    // if (clear) {
-    counter++;
-    Serial.print("counts : ");
-    Serial.println(counter);
-    clear = false;
-    // }
-    // clear = true;
-    // delay(10);
+  if (done) {
+    if (millis() - lastMillis > 400) {
+      lastMillis = millis();
+      //Serial.println("counting");
+      counter++;
+      if (counter == 1) {
+       // Serial.println("count1");
+        count1 = true;
+        count2 = false;
+        //done = false;
+      } else if (counter == 2) {
+       // Serial.println("count2");
+        count2 = true;
+        count1 = false;
+        counter = 0;
+
+      }
+      done = false;
+    }
+  }
 }
